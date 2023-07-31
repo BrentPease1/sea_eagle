28f96979d8f6aad1bb00ed4fbc2219889d49fd07
BrentPease1 <bspease@ncsu.edu>
2023-07-31 15:00:45 -0500

    cleaned up twitter script

--- Scripts/204 - Analysis - get twitter user information.R
+++ Scripts/204 - Analysis - get twitter user information.R
@@ -1,40 +1,6 @@
 library(academictwitteR)
 library(here)
 library(tidyverse)
-#set_bearer()
-#get_bearer()
-
-
-# -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- #
-# -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- #
-
-
-# learning how this works
-
-# tweets <-
-#   get_all_tweets(
-#     query = "#StellersSeaEagle",
-#     start_tweets = "2021-12-01T00:00:00Z",
-#     end_tweets = "2022-01-31T00:00:00Z",
-#     n = Inf,
-#     bind_tweets = FALSE,
-#     file = "stse_hashtag_tweets",
-#     data_path = here("Data/twitter/hashtag tweets")
-#   )
-# 
-# tweets <- bind_tweets(data_path = here("Data/twitter/hashtag tweets"), user = TRUE, output_format = 'tidy')
-# 
-# two <-   get_all_tweets(
-#   query = "Steller's Sea Eagle",
-#   start_tweets = "2021-12-01T00:00:00Z",
-#   end_tweets = "2022-01-31T00:00:00Z",
-#   n = Inf,
-#   bind_tweets = FALSE,
-#   file = "stse_name_tweets",
-#   data_path = here("Data/twitter/name tweets")
-# )
-# 
-# two <- bind_tweets(data_path = here("Data/twitter/name tweets"), user = TRUE, output_format = 'tidy')
 
 # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- #
 # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- #
@@ -74,34 +40,3 @@ pattern_search<- paste(these, collapse = "|")
 # grepl returns a logical vector TRUE/FALSE
 twit_table <- table(grepl(pattern_search, twitter$text))
 
-
-
-
-# # grep returns the indices of the matching elements
-# grep(pattern_search, twitter$text)
-# 
-# 
-# # to get the value names we use the argument value = TRUE of grep
-# grep(pattern_search, twitter$text, value = TRUE)
-# 
-# # str_detect returns logical vector 
-# str_detect(twitter$text, pattern_search)
-# 
-# # str_extract extracts the matching values and leaves NA in the not matching
-# str_extract(twitter$text, pattern_search)
-# 
-# 
-# 
-# # To apply these vector operation in a dataframe column basically you do the same, 
-# # unless you want to get the the value in the new column, then you add an `ifelse` statement
-# # Here is an example:
-# 
-# mtcars %>% 
-#   select(1) %>% 
-#   rownames_to_column("cars") %>% 
-#   mutate(new_grepl_TRUEFALSE = grepl(pattern_vehicles, cars)) %>% 
-#   mutate(new_grepl_value = ifelse(grepl(pattern_vehicles, cars), cars, NA_character_)) %>% 
-#   mutate(new_str_detect_TRUEFALSE = str_detect(cars, pattern_vehicles)) %>% 
-#   mutate(new_str_detect_value = ifelse(str_detect(cars, pattern_vehicles), cars, NA_character_)) %>% 
-#   mutate(new_str_extract = str_extract(cars, pattern_vehicles)) %>% 
-#   head()
